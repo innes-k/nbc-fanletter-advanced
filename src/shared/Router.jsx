dc514fe41a7ed6f08c6c56217cfec5bb661f8b22
innes-k <innes.kwak@gmail.com>
2024-02-17 02:36:53 +0900

    feat: login, profile route 생성

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,3 +1,5 @@
+import Login from "components/Login";
+import Profile from "components/Profile";
 import Detail from "pages/Detail";
 import Home from "pages/Home";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -9,6 +11,8 @@ export default function Router() {
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="*" element={<Navigate replace to="/" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
