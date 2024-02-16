58ef743831b2a4b497dc0c0c51769cb033cb16aa
innes-k <innes.kwak@gmail.com>
2024-02-17 03:42:10 +0900

    fix: 모든 페이지에서 NavBar 보이게 수정

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,5 +1,6 @@
 import Login from "components/Login";
 import Profile from "components/Profile";
+import Layout from "components/layout/Layout";
 import Detail from "pages/Detail";
 import Home from "pages/Home";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -7,6 +8,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 export default function Router() {
   return (
     <BrowserRouter>
+      <Layout />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
