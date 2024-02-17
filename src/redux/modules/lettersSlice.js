525b8a83e642b071c35169582b5c46f0229b8f2a
innes-k <innes.kwak@gmail.com>
2024-02-17 13:56:45 +0900

    feat: lettersSlice로 letters리듀서 옮김

--- src/redux/modules/lettersSlice.js
+++ src/redux/modules/lettersSlice.js
@@ -1,6 +1,31 @@
+import fakeData from "fakeData.json";
 const { createSlice } = require("@reduxjs/toolkit");
 
-const lettersSlice = createSlice();
+const initialState = fakeData;
+
+const lettersSlice = createSlice({
+  name: "letters",
+  initialState: initialState,
+  reducers: {
+    addLetter: (state, action) => {
+      const newLetter = action.payload;
+      return [newLetter, ...state];
+    },
+    deleteLetter: (state, action) => {
+      const letterId = action.payload;
+      return state.filter((letter) => letter.id !== letterId);
+    },
+    editLetter: (state, action) => {
+      const { id, editingText } = action.payload;
+      return state.map((letter) => {
+        if (letter.id === id) {
+          return { ...letter, content: editingText };
+        }
+        return letter;
+      });
+    },
+  },
+});
 
 export default lettersSlice.reducer;
-// export default letters.actions;
+export const { addLetter, deleteLetter, editLetter } = lettersSlice.actions;
